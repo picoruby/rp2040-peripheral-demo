ab0d16fd4937b4892d55959ab8986831734af9a7
HASUMI Hitoshi <hasumikin@gmail.com>
2023-04-04 14:57:47 +0900

    add ADC

--- mrblib/app.rb
+++ mrblib/app.rb
@@ -3,23 +3,25 @@ require "i2c"
 require "adafruit_pcf8523"
 require "time"
 require "uart"
+require "adc"
 
 ENV = Hash.new
 ENV['TZ'] = 'JST-9'
 
 uart = UART.new(unit: :RP2040_UART0, txd_pin: 0, rxd_pin: 1, baudrate: 115200)
 uart.write "\r\n\r\nInitializing...\r\n\r\n"
+sleep 1
 
 begin
   led = GPIO.new(25, GPIO::OUT)
   rtc = PCF8523.new(unit: :RP2040_I2C0, sda_pin: 4, scl_pin: 5)
+  adc = ADC.new(:temperature)
 rescue => e
   uart.write "Error: #{e}\r\n"
   uart.write "#{e.message}\r\n"
 end
 
-uart.write "\r\nCurrent time: #{rtc.current_time}\r\n"
-uart.write "Input time in `yyyy mm dd hh mm ss` or leave blank if you don't update\r\n"
+uart.write "Input time in `yyyy mm dd hh mm ss`\r\n"
 uart.write "> "
 line = ""
 while true
@@ -27,27 +29,21 @@ while true
   when nil
     next
   else
+    line << c
     uart.write c # echo back
-    if c != "\n"
-      line << c
-    else
+    if c == "\n"
       line.chomp!
-      if line.empty?
-        uart.write "Time not updated\r\n"
+      begin
+        time = Time.local(*line.split(" "))
+        rtc.current_time = time
+        Time.hwclock = time
+        uart.write "Time updated: #{rtc.current_time}\r\n"
         break
-      else
-        begin
-          time = Time.new(*line.split(" "))
-          rtc.current_time = time
-          Time.hwclock = time
-          uart.write "Time updated: #{rtc.current_time}\r\n"
-          break
-        rescue
-          uart.write "Invalid time format\r\n"
-          uart.write "(again)> "
-          line.clear
-          next
-        end
+      rescue
+        uart.write "Invalid time format\r\n"
+        uart.write "(again)> "
+        line.clear
+        next
       end
     end
   end
@@ -55,11 +51,15 @@ end
 
 uart.write "\r\nStarting loop\r\n\r\n"
 
+ADC_CONVERSION_FACTOR = 3.3 / (1<<12)
+
 while true
   sleep 1
   led.write 1
   uart.write "Current time: #{rtc.current_time}\r\n"
+  uart.write "Temperature: #{27 - (adc.read * ADC_CONVERSION_FACTOR - 0.706) / 0.001721} C\r\n"
   sleep 1
   led.write 0
 end
 
+ 27 - (ADC_Voltage - 0.706)/0.001721
